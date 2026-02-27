27ba86895637abb2a9f8e26987a50f4d35d191f7
bsucs <122378640+bsucs@users.noreply.github.com>
2026-02-26 20:09:38 -0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
   preview: {
     host: true,
-    port: 8080,
-    allowedHosts: [
-      "urchin-app-7gjmj.ondigitalocean.app"
-    ]
+    allowedHosts: true
   }
 })
